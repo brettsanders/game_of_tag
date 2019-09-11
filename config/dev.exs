09ae8b138e9e4a19e934eb6289a65b42e86219c9
Brett Sanders <brettrsanders@gmail.com>
2019-09-10 21:18:54 -0400

    Add GameState module, use for board height & width

--- config/dev.exs
+++ config/dev.exs
@@ -60,7 +60,7 @@ config :game_of_tag, GameOfTagWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/game_of_tag_web/{live,views}/.*(ex)$",
+      ~r"lib/game_of_tag_web/{controllers,live,views}/.*(ex)$",
       ~r"lib/game_of_tag_web/templates/.*(eex)$"
     ]
   ]
--- lib/game_of_tag_web/controllers/page_controller.ex
+++ lib/game_of_tag_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule GameOfTagWeb.PageController do
   use GameOfTagWeb, :controller
+  alias GameOfTag.GameState
 
+  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(
+      conn,
+      "index.html",
+      screen_height: GameState.screen_height(),
+      screen_width: GameState.screen_width()
+    )
   end
 end
